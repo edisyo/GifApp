d183c2f3dd065b73c6cf0a734f1accd389fef1ba
Edisyo <eduards.jes@gmail.com>
2026-08-17 20:46:03 +0300

    chore: test api call via button press

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+const giphyApiKey = String.fromEnvironment('GIPHY_API_KEY');
 
 void main(){
-  const check = String.fromEnvironment('GIPHY_API_KEY');
-  print("Key length: ${check.length}");
+  print("Key length: ${giphyApiKey.length}");
   runApp(const GifApp());
 } 
 
@@ -13,16 +13,41 @@ class GifApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Explore Giphy",
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 156, 122, 213),
+        appBar: AppBar(
+          title: Text('Testing'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: testCall,
+        backgroundColor: Colors.deepPurpleAccent,
+        hoverColor: Colors.deepOrange,
+        focusColor: Colors.blue,
+        child: const Icon(Icons.search),
+        ),
+        
       ),
     );
   }
 }
 
 Future<void> testCall() async {
-  //var uri = Uri.https(authority)
+  print('Calling the API...');
+  var uri = Uri.https(
+    "api.giphy.com",
+    "/v1/gifs/search",
+    {
+      'api_key': giphyApiKey,
+      'q': 'cats',
+      'limit': '4'
+    });
+
+  final response = await http.get(uri);
+  final body = response.body;
+  print('Response status: ${response.statusCode}');
+  print('Response body: $body');
 }
